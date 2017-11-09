0ebdf765e7aac5d044c447381d32348f3442a044
jaerickson <31251673+jaerickson@users.noreply.github.com>
2017-11-09 10:00:37 -0800

    Update webapp.py

--- webapp.py
+++ webapp.py
@@ -14,8 +14,21 @@ def get_state_options():
             s.append(c["State"])
             options += Markup("<option value=\"" + c["State"] + "\">" + c["State"] + "</option>")
     return options
+def state_fun_fact(states)
+    with open('county_demographics.json') as demographics_data:
+        counties = json.load(demographics_data)
+    s = counties[0]["State"]
+    d = counties[0]["County"]
+    most_women_owned = counties[0]["Employment"]["Firms"]["Women-Owned"]
+    for c in counties:
+        if c["State"] = states:
+            if c["Employment"]["Firms"]["Women-Owned"] > most_women_owned:
+                most_women_owned = c["Employment"]["Firms"]["Women-Owned"]
+                s = c["State"]
+                d = c["County"]
+    return d +" is the highest percentage women owned of "+ s +" "+ str(most_women_owned) +"% women owned"
 @app.route("/")
 def render_home():   
-    return render_template('home.html',states=get_state_options())
+    return render_template('home.html',states=get_state_options(), fact=Stae_fun_fact(states))
 if __name__ == '__main__':
     app.run(debug=False, port=54321)
