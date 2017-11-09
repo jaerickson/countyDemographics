0eb4f074e039168539e544b77d69e73732bba0c2
jaerickson <31251673+jaerickson@users.noreply.github.com>
2017-11-09 08:51:17 -0800

    Update webapp.py

--- webapp.py
+++ webapp.py
@@ -3,19 +3,19 @@ import os
 import json
 
 app = Flask(__name__)
-@app.route("/")
-def render_home():   
-    return render_template('home.html',states=get_state_options())
 
 def get_state_options():
     with open('county_demographics.json') as demographics_data:
         counties = json.load(demographics_data)
     options = ""
-    s = []
+    currentState = counties[0]["State"]
     for c in counties:
-        if c["State"] not in s:
-            s.append(c["State"])
-        options += Markup("<option value=\"" + s + "\">" + s + "</option>")
+        if c["State"] != currentState:
+            options += c["State"]
+            options += Markup("<option value=\"" + s + "\">" + s + "</option>")
     return options
+@app.route("/")
+def render_home():   
+    return render_template('home.html',states=get_state_options())
 if __name__ == '__main__':
     app.run(debug=False, port=54321)
